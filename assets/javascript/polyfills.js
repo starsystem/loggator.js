ce0b964691a7bf359455a735468764874e130af0
Alex Petrosh <petrosh@users.noreply.github.com>
2016-12-21 16:13:55 +0100

    Add `template` polyfills

--- assets/javascript/polyfills.js
+++ assets/javascript/polyfills.js
@@ -4,3 +4,4 @@
 {% include_relative polyfills/promise.js %}
 {% include_relative polyfills/fetch.js %}
 {% include_relative polyfills/localStorage.js %}
+{% include_relative polyfills/template.js %}
