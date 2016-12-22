807f7965d231e48f11dcd85446c0510e947e5cb5
Alex Petrosh <petrosh@users.noreply.github.com>
2016-12-22 18:30:17 +0100

    Remove observer defer polyfill

--- assets/javascript/polyfills.js
+++ assets/javascript/polyfills.js
@@ -1,7 +1,6 @@
 ---
 ---
 
-{% include_relative polyfills/intersection-observer.js %}
 {% include_relative polyfills/promise.js %}
 {% include_relative polyfills/fetch.js %}
 {% include_relative polyfills/localStorage.js %}
