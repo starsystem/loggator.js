8f50a62edee79c7b16b8970352ef1b7b62434ce8
Alex Petrosh <petrosh@users.noreply.github.com>
2016-12-22 18:02:41 +0100

    Try intersection-observer polyfill

--- assets/javascript/polyfills.js
+++ assets/javascript/polyfills.js
@@ -1,6 +1,7 @@
 ---
 ---
 
+{% include_relative polyfills/intersection-observer.js %}
 {% include_relative polyfills/promise.js %}
 {% include_relative polyfills/fetch.js %}
 {% include_relative polyfills/localStorage.js %}
