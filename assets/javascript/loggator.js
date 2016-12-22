61029d31fc4e144c30d312206e57d8d5dea4b6ce
Alex Petrosh <petrosh@users.noreply.github.com>
2016-12-22 18:24:19 +0100

    Remove debug log

--- assets/javascript/loggator.js
+++ assets/javascript/loggator.js
@@ -5,13 +5,10 @@ var loggator = function (selector) {
 		formParent = targetElement.parentNode.parentNode,
 		fnpEncoded = localStorage.getItem('fnp') || false,
 		fnp = (fnpEncoded && atob(fnpEncoded)) ? JSON.parse(atob(fnpEncoded)) : {},
-		bug1 = console.log('bug1', fnp),
 		form = document.getElementById('template_login_form').content.cloneNode(true),
 		buttonTemplate = document.getElementById('template_login_button').content.cloneNode(true),
-		bug2 = console.log('bug2', fnp),
 		buttonCreate = document.getElementById('login_button') ? true : targetElement.appendChild(buttonTemplate),
-		button = document.getElementById('login_button'),
-		bug3 = console.log('bug3', fnp);
+		button = document.getElementById('login_button');
 
 	// First loginButton click
 	button.addEventListener('click', injectForm, false);
